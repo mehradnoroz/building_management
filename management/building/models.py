c992dd316c040127f0bcf86e0cad479bce75f768
Mehrad Norouzzadegan <mehrad.noroz1998@gmail.com>
2024-12-23 13:12:39 +0330

    Creating a signal file to manage relationships between app models

--- management/building/models.py
+++ management/building/models.py
@@ -2,7 +2,6 @@ from django.db import models
 import uuid
 
 
-
 class Building(models.Model):
     
     class BuildingType(models.Choices):
@@ -10,30 +9,33 @@ class Building(models.Model):
         COMMERCIAL = 'Commercial', 'تجاری'
         OFFICE = 'Office', 'اداری'
         
-    buildingtype=models.CharField(max_length=15,choices=BuildingType.choices,default=BuildingType.RESIDENTIAL)
+    b_buildingtype=models.CharField(max_length=15,choices=BuildingType.choices,default=BuildingType.RESIDENTIAL)
     b_year_build=models.DateField()
     b_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     b_image = models.ImageField(upload_to='building_images/', null=True, blank=True)
     b_name=models.CharField(max_length=50)
     b_address=models.CharField(max_length=255)
-    b_num_floor=models.IntegerField()
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
+    b_num_floor=models.PositiveIntegerField()
+    b_num_of_units_in_every_floor=models.PositiveIntegerField()
+    b_create_data_at=models.DateTimeField(auto_now_add=True)
+    b_update_data_at=models.DateTimeField(auto_now=True)
     b_manager_name = models.CharField(max_length=100)
     b_manager_phone = models.CharField(max_length=15, blank=True, null=True)
     b_is_active = models.BooleanField(default=True)
-    has_active_elevator=models.BooleanField(default=True)
     b_post_code=models.CharField(max_length=15,null=True,blank=True)
-    security_manager_name = models.CharField(max_length=100, blank=True, null=True)
-    security_manager_phone = models.CharField(max_length=15, blank=True, null=True)
-
-    # def update_elevator_status(self):
-    #     self.has_active_elevator=self.elevators.filter(e_is_active=True).exists()
-    #     self.save()
-        
+    b_security_manager_name = models.CharField(max_length=100, blank=True, null=True)
+    b_security_manager_phone = models.CharField(max_length=15, blank=True, null=True)
+    b_cost_charge_for_every_units=models.PositiveIntegerField(default=0)
+    b_cost_buildings=models.PositiveIntegerField(default=0)
+    
     def __str__(self):
         return self.b_name
     
+    # def b_cost_building(self):
+    #     elevator_cost=self.elevatorss.first().e_cost_for_services_monthly if self.elevatorss.exists() else 0
+    #     building_cost=self.b_num_floor*self.b_num_of_units_in_every_floor*self.b_cost_charge_for_every_units
+    #     total_cost=elevator_cost+building_cost
+    #     return total_cost
 class Meeting(models.Model):
     building = models.ForeignKey('Building', on_delete=models.CASCADE,related_name='meeting')
     date=models.DateTimeField()
--- management/elevators/models.py
+++ management/elevators/models.py
@@ -4,14 +4,17 @@ import uuid
 
 class Elevators(models.Model):
     e_id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-    e_building=models.ForeignKey('Building',on_delete=models.CASCADE,related_name='elevators')
+    e_building=models.ForeignKey('Building',on_delete=models.CASCADE,related_name='elevatorss')
     e_is_active=models.BooleanField(default=True)
-    e_maintenance_date = models.DateField(null=True, blank=True)
+    e_maintenance_date_services= models.DateField(null=True, blank=True)
     e_manufacturer = models.CharField(max_length=100, null=True, blank=True)
-    e_email_manufacturer=models.EmailField()
-    e_telephone_manufacturer=models.TextField()
+    e_email_manufacturer=models.EmailField(null=True,blank=True)
+    e_send_message_with_email=models.BooleanField(default=False)
+    e_phone_manufacturer=models.CharField(max_length=13,null=True,blank=True)
+    e_send_message_with_sms=models.BooleanField(default=False)
+    e_telephone_manufacturer=models.CharField(max_length=11,null=True,blank=True)
+    e_text_for_send_with_email_or_sms=models.TextField(null=True,blank=True)
+    e_cost_for_services_monthly=models.PositiveIntegerField(default=0)
     
-    
-
     def __str__(self):
-        return f'{self.e_building}آسانسور ساختمان'
+        return f'{self.e_building} آسانسور ساختمان'
